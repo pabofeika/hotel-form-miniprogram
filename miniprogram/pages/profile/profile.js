aa07bba7151b03105dccf2ec3baf15bd2ed91497
Leon <skyworth@MacBook-Pro-4.local>
2026-05-19 21:38:42 +0800

    fix: profile page - add theme support + theme toggle

--- miniprogram/pages/profile/profile.js
+++ miniprogram/pages/profile/profile.js
@@ -1,16 +1,32 @@
 const cache = require('../../utils/cache');
+const theme = require('../../utils/theme');
 
 Page({
   data: {
     userName: '酒店配置用户',
     userDesc: '创维商用智慧酒店',
+    theme: 'light',
+  },
+
+  onLoad() {
+    this.setData({ theme: theme.getTheme() });
   },
 
   onShow() {
+    this.setData({ theme: theme.getTheme() });
+    theme.syncTabBar(theme.getTheme());
+
     const app = getApp();
     if (app.globalData && app.globalData.userInfo) {
       this.setData({ userName: app.globalData.userInfo.nickname || '酒店配置用户' });
     }
+  },
+
+  toggleTheme() {
+    theme.toggleTheme();
+    const t = theme.getTheme();
+    this.setData({ theme: t });
+    theme.syncTabBar(t);
   },
 
   goToRecords() {
